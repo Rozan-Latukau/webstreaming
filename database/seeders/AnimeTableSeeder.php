77d14d46024782cf10b86146a5c28a9c4eff7d7b
Rozan-Latukau <iyanlatu@gmail.com>
2024-01-16 16:59:39 +0700

    feat:integration data

--- database/seeders/AnimeTableSeeder.php
+++ database/seeders/AnimeTableSeeder.php
@@ -20,7 +20,7 @@ class AnimeTableSeeder extends Seeder
                 'genre' => 'Adventure. Fantasy, comedi',
                 'video_url' => 'https://www.youtube.com/watch?v=nOgo2xhPZm8',
                 'thumbnail' => 'https://i.pinimg.com/736x/22/3f/77/223f775de1ee3781c67ee0a4d6d26a13.jpg',
-                'rating' => 8.0,
+                'rating' => 7.0,
                 'is_featured' => 1,
             ],
             [
@@ -29,7 +29,7 @@ class AnimeTableSeeder extends Seeder
                 'genre' => 'Action, fantasy',
                 'video_url' => 'https://www.youtube.com/watch?v=nOgo2xhPZm8',
                 'thumbnail' => 'https://i.pinimg.com/originals/d4/8a/70/d48a708798e36fcbab438b5b11de0ec1.jpg',
-                'rating' => 9.0,
+                'rating' => 8.0,
                 'is_featured' => 0,
             ],
             [
@@ -38,7 +38,7 @@ class AnimeTableSeeder extends Seeder
                 'genre' => 'dark, fantasy',
                 'video_url' => 'https://www.youtube.com/watch?v=nOgo2xhPZm8',
                 'thumbnail' => 'https://i.pinimg.com/564x/0d/ca/eb/0dcaeb325ca673fce063b78bb3a54f1b.jpg',
-                'rating' => 7.5,
+                'rating' => 7.8,
                 'is_featured' => 0,
             ]
         ];
